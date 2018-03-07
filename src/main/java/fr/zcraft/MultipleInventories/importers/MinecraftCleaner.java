590fe9c49bb9777e863dbaeca01667bc15eecd32
Amaury Carrade <amaury@carrade.eu>
2018-03-07 13:24:31 +0100

    Fixed code style in Minecraft Cleaner.

--- src/main/java/fr/zcraft/MultipleInventories/importers/MinecraftCleaner.java
+++ src/main/java/fr/zcraft/MultipleInventories/importers/MinecraftCleaner.java
@@ -9,38 +9,51 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Map;
 
+
 /**
  * This class provides utilities to "fix" Minecraft memory leaks that will arise during importation
  */
-public class MinecraftCleaner {
+public class MinecraftCleaner
+{
     static private Class criterionTrigger;
     static private Method criterionTriggerGetIterable;
 
     static private boolean hasFailed;
 
-    static private Iterable getAllTriggers() throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
-        if(criterionTrigger == null) {
+    static private Iterable getAllTriggers() throws ClassNotFoundException, InvocationTargetException, IllegalAccessException
+    {
+        if (criterionTrigger == null)
+        {
             criterionTrigger = Reflection.getMinecraftClassByName("CriterionTriggers");
             criterionTriggerGetIterable = Reflection.findMethod(criterionTrigger, null, Iterable.class, Modifier.STATIC);
         }
         return (Iterable) criterionTriggerGetIterable.invoke(null);
     }
 
-    static private Map findCriterionMap(Object o) throws NoSuchFieldException, IllegalAccessException {
-        Field f = Reflection.getField(o.getClass(), Map.class);
+    static private Map findCriterionMap(final Object o) throws NoSuchFieldException, IllegalAccessException
+    {
+        final Field f = Reflection.getField(o.getClass(), Map.class);
         f.setAccessible(true);
         return (Map) f.get(o);
     }
 
-    static public void cleanup() {
-        if(hasFailed) return;
-        try {
-            for(Object o : getAllTriggers()) {
-                try {
+    static public void cleanup()
+    {
+        if (hasFailed) return;
+
+        try
+        {
+            for (Object o : getAllTriggers())
+            {
+                try
+                {
                     findCriterionMap(o).clear();
-                } catch(NoSuchFieldException ignored) {}
+                }
+                catch (NoSuchFieldException ignored) {}
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             PluginLogger.error("Failed to cleanup Minecraft Leaked memory", e);
             hasFailed = true;
         }
